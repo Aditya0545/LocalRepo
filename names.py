5e4d1aa72eea0dd2fb958f8d4809b4cceaabd4de
Aditya <adityakumar2019.ak@gmail.com>
2023-10-03 00:56:53 +0530

    add_practiical

---
+++ names.py
@@ -0,0 +1,10 @@
+'''Write a program that sorts a list of names in alphabetical order and then in reverse alphabetical order'''
+
+# Name: Aditya kumar
+# ID : 22BTCSE067
+
+names = ["Elayne", "Aditya", "Fariha", "Chandani", "Gautam", "Dinesh", "Hazel", "Bittu"]
+names.sort()
+print(f"names in alphabetical order {names}")
+names.reverse()
+print(f"names in reverse alphabetical order {names}")
